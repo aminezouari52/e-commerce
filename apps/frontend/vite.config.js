8ff59b59839f8fcccca8da7eabb9ca37a65964af
Amine Zouari <zouariamine52@gmail.com>
2024-12-15 14:35:29 +0100

    fix: update build command

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
